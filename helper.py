367b7f8322bb94830c2355e6fad3c5cacab86d95
Suzanne den Hollander <susene@geeks.nl>
2024-01-30 09:32:41 +0100

    functies toegevoegd

--- helper.py
+++ helper.py
@@ -1,8 +1,8 @@
 # Les 07 7.11 helper file maken
-tekst = "header"
-lengte = len(tekst) + 4
-print()
-print(lengte * "*")
-print(f"* {tekst} *")
-print(lengte * "*")
-print()
+def decoreer(tekst=""):
+    lengte = len(tekst) + 4
+    print()
+    print(lengte * "*")
+    print(f"* {tekst} *")
+    print(lengte * "*")
+    print()
--- tijdelijk.py
+++ tijdelijk.py
@@ -1,20 +1,26 @@
 #huiswerk Les 07
-Prijzen = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5
-}
-Aanbieding = (Prijzen["vanille"] * 0.8)
-Reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {Aanbieding}")
-print (Reclame_tekst)
-# print(f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {Aanbieding}") - vind ik fijner dan eerst via reclame_tekst. Maar opgave lijkt te zeggen dat het via die extra regel moet
-# Ik heb als uitkomst 3.2 zonder alle nullen. Dus ik index maar op de 2 om de volgende vraag te voldoen en behoud de eerste print opdracht ook. 
-Reclame_tekst2 = (Reclame_tekst[:61])
-Reclame_tekst3 = (Reclame_tekst2.upper())
-Reclame_tekst4 = ("VANDAAG", "IN", "DE", "AANBIEDING",":", "VANILLE-IJS",", ", "1", "LITER", "-", "SLECHTS", "€", "3",".")
-El = (Reclame_tekst4)
-for i in El:
-    if len(i) < 5:
-        print(i.lower())
-    else:
-        print(i.upper())
+#importeren functie vanuit ander bestand; les 08
+from helper import decoreer
+
+def print_aanbieding():
+    Prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
+    Aanbieding = (Prijzen["aardbei"] * 0.8)
+    Reclame_tekst = (f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {Aanbieding}")
+    
+    Reclame_tekst2 = (Reclame_tekst[:63])
+    
+    Reclame_tekst3 = (Reclame_tekst2.upper())
+
+    Reclame_tekst4 = ("VANDAAG", "IN", "DE", "AANBIEDING",":", "AARDBEI-IJS",", ", "1", "LITER", "-", "SLECHTS", "€", "2.40",".")
+    for El in Reclame_tekst4:
+        if len(El) < 5:
+            print(El.lower())
+        else:
+            print(El.upper())
+
+decoreer("Aanbieding")
+print_aanbieding()
